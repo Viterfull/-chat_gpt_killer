2b88452b2e0995753fb2481bfba8834044d153a6
Dima <globin.do@phystech.edu>
2023-05-05 05:39:44 +0300

    Done full machine translation model

---
+++ gpt_kernel/model_trainer.py
@@ -0,0 +1,286 @@
+import numpy as np
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+from transformer import Transformer
+
+def create_masks(eng_batch, kn_batch):
+
+    max_sequence_length = 200
+    NEG_INFTY = -1e9
+    num_sentences = len(eng_batch)
+    look_ahead_mask = torch.full([max_sequence_length, max_sequence_length] , True)
+    look_ahead_mask = torch.triu(look_ahead_mask, diagonal=1)
+    encoder_padding_mask = torch.full([num_sentences, max_sequence_length, max_sequence_length] , False)
+    decoder_padding_mask_self_attention = torch.full([num_sentences, max_sequence_length, max_sequence_length] , False)
+    decoder_padding_mask_cross_attention = torch.full([num_sentences, max_sequence_length, max_sequence_length] , False)
+
+    for idx in range(num_sentences):
+      
+      eng_sentence_length, kn_sentence_length = len(eng_batch[idx]), len(kn_batch[idx])
+      eng_chars_to_padding_mask = np.arange(eng_sentence_length + 1, max_sequence_length)
+      kn_chars_to_padding_mask = np.arange(kn_sentence_length + 1, max_sequence_length)
+      encoder_padding_mask[idx, :, eng_chars_to_padding_mask] = True
+      encoder_padding_mask[idx, eng_chars_to_padding_mask, :] = True
+      decoder_padding_mask_self_attention[idx, :, kn_chars_to_padding_mask] = True
+      decoder_padding_mask_self_attention[idx, kn_chars_to_padding_mask, :] = True
+      decoder_padding_mask_cross_attention[idx, :, eng_chars_to_padding_mask] = True
+      decoder_padding_mask_cross_attention[idx, kn_chars_to_padding_mask, :] = True
+
+    encoder_self_attention_mask = torch.where(encoder_padding_mask, NEG_INFTY, 0)
+    decoder_self_attention_mask =  torch.where(look_ahead_mask + decoder_padding_mask_self_attention, NEG_INFTY, 0)
+    decoder_cross_attention_mask = torch.where(decoder_padding_mask_cross_attention, NEG_INFTY, 0)
+
+    return encoder_self_attention_mask, decoder_self_attention_mask, decoder_cross_attention_mask
+
+
+class TextDataset(Dataset):
+
+    def __init__(self, english_sentences, kannada_sentences):
+
+        self.english_sentences = english_sentences
+        self.kannada_sentences = kannada_sentences
+
+    def __len__(self):
+        
+        return len(self.english_sentences)
+
+    def __getitem__(self, idx):
+        
+        return self.english_sentences[idx], self.kannada_sentences[idx]
+
+def is_valid_tokens(sentence, vocab):
+
+    for token in list(set(sentence)):
+        
+        if token not in vocab:
+            return False
+        
+    return True
+
+def is_valid_length(sentence, max_sequence_length):
+    return len(list(sentence)) < (max_sequence_length - 1)
+
+class ModelTrainer():
+
+    def __init__(self):
+        
+        pass
+    
+    def train(self, model, train_loader):
+        
+        criterian = nn.CrossEntropyLoss(ignore_index=trg_to_index[model.PADDING_TOKEN],
+                            reduction='none')
+
+        # When computing the loss, we are ignoring cases when the label is the padding token
+        for params in transformer.parameters():
+            if params.dim() > 1:
+                nn.init.xavier_uniform_(params)
+
+        optim = torch.optim.Adam(transformer.parameters(), lr=1e-4)
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+        model.train()
+        model.to(device)
+        num_epochs = 10
+
+        for epoch in range(num_epochs):
+
+            print(f"Epoch {epoch}")
+            iterator = iter(train_loader)
+
+            for batch_num, batch in enumerate(iterator):
+
+                model.train()
+                src_batch, trg_batch = batch
+                encoder_self_attention_mask, decoder_self_attention_mask, decoder_cross_attention_mask = create_masks(src_batch, trg_batch)
+                optim.zero_grad()
+
+                trg_predictions = model(src_batch,
+                                        trg_batch,
+                                        encoder_self_attention_mask.to(device), 
+                                        decoder_self_attention_mask.to(device), 
+                                        decoder_cross_attention_mask.to(device),
+                                        enc_start_token=False,
+                                        enc_end_token=False,
+                                        dec_start_token=True,
+                                        dec_end_token=True)
+                
+                labels = model.decoder.sentence_embedding.batch_tokenize(trg_batch, start_token=False, end_token=True)
+                
+                loss = criterian(
+                    trg_predictions.view(-1, model.trg_vocab_size).to(device),
+                    labels.view(-1).to(device)
+                ).to(device)
+                
+                valid_indicies = torch.where(labels.view(-1) == trg_to_index[model.PADDING_TOKEN], False, True)
+                loss = loss.sum() / valid_indicies.sum()
+                loss.backward()
+                optim.step()
+                
+                #train_losses.append(loss.item())
+                
+                if batch_num % 100 == 0:
+                    
+                    print(f"Iteration {batch_num} : {loss.item()}")
+                    print(f"English: {src_batch[0]}")
+                    print(f"Kannada Translation: {trg_batch[0]}")
+
+                    trg_sentence_predicted = torch.argmax(trg_predictions[0], axis=1)
+                    predicted_sentence = ""
+
+                    for idx in trg_sentence_predicted:
+                        
+                        if idx == trg_to_index[model.END_TOKEN]:
+                            break
+                        predicted_sentence += index_to_trg[idx.item()]
+                        
+                    print(f"Target Prediction: {predicted_sentence}")
+
+
+                    model.eval()
+                    trg_sentence = ("",)
+                    src_sentence = ("should we go to the mall?",)
+
+                    for word_counter in range(model.max_sequence_length):
+                        
+                        encoder_self_attention_mask, decoder_self_attention_mask, decoder_cross_attention_mask = create_masks(src_sentence, trg_sentence)
+                        predictions = model(src_sentence,
+                                                trg_sentence,
+                                                encoder_self_attention_mask.to(device), 
+                                                decoder_self_attention_mask.to(device), 
+                                                decoder_cross_attention_mask.to(device),
+                                                enc_start_token=False,
+                                                enc_end_token=False,
+                                                dec_start_token=True,
+                                                dec_end_token=False)
+                        next_token_prob_distribution = predictions[0][word_counter] # not actual probs
+                        next_token_index = torch.argmax(next_token_prob_distribution).item()
+                        next_token = index_to_trg[next_token_index]
+                        trg_sentence = (trg_sentence[0] + next_token, )
+                        if next_token == model.END_TOKEN:
+                            break
+                    
+                    print(f"Evaluation translation (should we go to the mall?) : {trg_sentence}")
+                    print("-------------------------------------------")
+
+        return
+
+if __name__ == "__main__":
+    max_sequence_length = 200
+
+    START_TOKEN = '<START>'
+    PADDING_TOKEN = '<PADDING>'
+    END_TOKEN = '<END>'
+
+    src_file = "english.txt"
+    trg_file = "kannada.txt"
+
+    with open(src_file, 'r') as file:
+        src_sentences = file.readlines()
+    with open(trg_file, 'r') as file:
+        trg_sentences = file.readlines()
+
+    # trg_vocab = set()
+    # src_vocab = set()
+
+    # for trg_sentence in trg_sentences:
+    #     trg_vocab.update(trg_sentence)
+
+    # for src_sentence in src_sentences:
+    #     src_vocab.update(src_sentence)
+
+    # trg_vocab = list(trg_vocab)
+    # src_vocab = list(src_vocab)
+
+    # trg_vocab.append(START_TOKEN)
+    # trg_vocab.append(END_TOKEN)
+    # trg_vocab.append(PADDING_TOKEN)
+
+    # src_vocab.append(START_TOKEN)
+    # src_vocab.append(END_TOKEN)
+    # src_vocab.append(PADDING_TOKEN)
+
+    trg_vocab = [START_TOKEN, ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', 
+                      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', '<', '=', '>', '?', 'ˌ', 
+                      'ँ', 'ఆ', 'ఇ', 'ా', 'ి', 'ీ', 'ు', 'ూ', 
+                      'ಅ', 'ಆ', 'ಇ', 'ಈ', 'ಉ', 'ಊ', 'ಋ', 'ೠ', 'ಌ', 'ಎ', 'ಏ', 'ಐ', 'ಒ', 'ಓ', 'ಔ', 
+                      'ಕ', 'ಖ', 'ಗ', 'ಘ', 'ಙ', 
+                      'ಚ', 'ಛ', 'ಜ', 'ಝ', 'ಞ', 
+                      'ಟ', 'ಠ', 'ಡ', 'ಢ', 'ಣ', 
+                      'ತ', 'ಥ', 'ದ', 'ಧ', 'ನ', 
+                      'ಪ', 'ಫ', 'ಬ', 'ಭ', 'ಮ', 
+                      'ಯ', 'ರ', 'ಱ', 'ಲ', 'ಳ', 'ವ', 'ಶ', 'ಷ', 'ಸ', 'ಹ', 
+                      '಼', 'ಽ', 'ಾ', 'ಿ', 'ೀ', 'ು', 'ೂ', 'ೃ', 'ೄ', 'ೆ', 'ೇ', 'ೈ', 'ೊ', 'ೋ', 'ೌ', '್', 'ೕ', 'ೖ', 'ೞ', 'ೣ', 'ಂ', 'ಃ', 
+                      '೦', '೧', '೨', '೩', '೪', '೫', '೬', '೭', '೮', '೯', PADDING_TOKEN, END_TOKEN]
+
+    src_vocab = [START_TOKEN, ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', 
+                        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                        ':', '<', '=', '>', '?', '@',
+                        '[', '\\', ']', '^', '_', '`', 
+                        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+                        'y', 'z', 
+                        '{', '|', '}', '~', PADDING_TOKEN, END_TOKEN]
+
+    index_to_trg = {k:v for k,v in enumerate(trg_vocab)}
+    trg_to_index = {v:k for k,v in enumerate(trg_vocab)}
+    index_to_src = {k:v for k,v in enumerate(src_vocab)}
+    src_to_index = {v:k for k,v in enumerate(src_vocab)}
+
+    TOTAL_SENTENCES = 200000
+    src_sentences = src_sentences[:TOTAL_SENTENCES]
+    trg_sentences = trg_sentences[:TOTAL_SENTENCES]
+    src_sentences = [sentence.rstrip('\n').lower() for sentence in src_sentences]
+    trg_sentences = [sentence.rstrip('\n') for sentence in trg_sentences]
+
+    valid_sentence_indicies = []
+
+    for index in range(len(trg_sentences)):
+
+        trg_sentence, src_sentence = trg_sentences[index], src_sentences[index]
+        if is_valid_length(trg_sentence, max_sequence_length) \
+        and is_valid_length(src_sentence, max_sequence_length) \
+        and is_valid_tokens(trg_sentence, trg_vocab):
+            valid_sentence_indicies.append(index)
+
+    trg_sentences = [trg_sentences[i] for i in valid_sentence_indicies]
+    src_sentences = [src_sentences[i] for i in valid_sentence_indicies]
+
+    d_model = 512
+    batch_size = 30
+    ffn_hidden = 2048
+    num_heads = 8
+    drop_prob = 0.1
+    num_layers = 1
+    max_sequence_length = 200
+    kn_vocab_size = len(trg_vocab)
+
+    transformer = Transformer(d_model, 
+                            ffn_hidden,
+                            num_heads, 
+                            drop_prob, 
+                            num_layers, 
+                            max_sequence_length,
+                            kn_vocab_size,
+                            src_to_index,
+                            trg_to_index,
+                            START_TOKEN, 
+                            END_TOKEN, 
+                            PADDING_TOKEN)
+
+    dataset = TextDataset(src_sentences, trg_sentences)
+
+    train_loader = DataLoader(dataset, batch_size)
+    iterator = iter(train_loader)
+
+    for batch_num, batch in enumerate(iterator):
+        print(batch)
+        if batch_num > 3:
+            break
+
+    trainer = ModelTrainer()
+
+    trainer.train(transformer, train_loader)
---
+++ gpt_kernel/transformer.py
@@ -0,0 +1,416 @@
+import numpy as np
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+
+def create_masks(eng_batch, kn_batch):
+
+    max_sequence_length = 200
+    NEG_INFTY = -1e9
+    num_sentences = len(eng_batch)
+
+    look_ahead_mask = torch.full([max_sequence_length, max_sequence_length] , True)
+    look_ahead_mask = torch.triu(look_ahead_mask, diagonal=1)
+    
+    encoder_padding_mask = torch.full([num_sentences, max_sequence_length, max_sequence_length] , False)
+    decoder_padding_mask_self_attention = torch.full([num_sentences, max_sequence_length, max_sequence_length] , False)
+    decoder_padding_mask_cross_attention = torch.full([num_sentences, max_sequence_length, max_sequence_length] , False)
+
+    for idx in range(num_sentences):
+      
+      eng_sentence_length, kn_sentence_length = len(eng_batch[idx]), len(kn_batch[idx])
+      eng_chars_to_padding_mask = np.arange(eng_sentence_length + 1, max_sequence_length)
+      kn_chars_to_padding_mask = np.arange(kn_sentence_length + 1, max_sequence_length)
+      
+      encoder_padding_mask[idx, :, eng_chars_to_padding_mask] = True
+      encoder_padding_mask[idx, eng_chars_to_padding_mask, :] = True
+
+      decoder_padding_mask_self_attention[idx, :, kn_chars_to_padding_mask] = True
+      decoder_padding_mask_self_attention[idx, kn_chars_to_padding_mask, :] = True
+      decoder_padding_mask_cross_attention[idx, :, eng_chars_to_padding_mask] = True
+      decoder_padding_mask_cross_attention[idx, kn_chars_to_padding_mask, :] = True
+
+    encoder_self_attention_mask = torch.where(encoder_padding_mask, NEG_INFTY, 0)
+    decoder_self_attention_mask =  torch.where(look_ahead_mask + decoder_padding_mask_self_attention, NEG_INFTY, 0)
+    decoder_cross_attention_mask = torch.where(decoder_padding_mask_cross_attention, NEG_INFTY, 0)
+
+    return encoder_self_attention_mask, decoder_self_attention_mask, decoder_cross_attention_mask
+
+
+def get_device():
+    return torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+def scaled_dot_product(q, k, v, mask = None):
+    
+    d_k = q.size()[-1]
+
+    scaled = torch.matmul(q, k.transpose(-1, -2)) / np.sqrt(d_k)
+
+    if mask is not None:
+
+        # print(f"MASK IN NOT NONE, ITS SHAPE: {mask.size()}")
+
+        scaled = scaled.permute(1, 0, 2, 3) + mask
+        scaled = scaled.permute(1, 0, 2, 3)
+
+    attention = F.softmax(scaled, dim=-1)
+    out = torch.matmul(attention, v)
+
+    return out, attention
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, max_sequence_length):
+
+        super().__init__()
+        self.max_sequence_length = max_sequence_length
+        self.d_model = d_model
+
+    def forward(self):
+
+        even_i = torch.arange(0, self.d_model, 2).float()
+        denominator = torch.pow(10000, even_i/self.d_model)
+        position = (torch.arange(self.max_sequence_length).reshape(self.max_sequence_length, 1))
+        
+        even_PE = torch.sin(position / denominator)
+        odd_PE = torch.cos(position / denominator)
+        stacked = torch.stack([even_PE, odd_PE], dim=2)
+        
+        PE = torch.flatten(stacked, start_dim=1, end_dim=2)
+
+        return PE
+
+class SentenceEmbedding(nn.Module):
+
+    def __init__(self, max_sequence_length, d_model, language_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN):
+        
+        super().__init__()
+        self.vocab_size = len(language_to_index)
+        self.max_sequence_length = max_sequence_length
+        self.embedding = nn.Embedding(self.vocab_size, d_model)
+        self.language_to_index = language_to_index
+        self.position_encoder = PositionalEncoding(d_model, max_sequence_length)
+        self.dropout = nn.Dropout(p=0.1)
+        
+        self.START_TOKEN = START_TOKEN
+        self.END_TOKEN = END_TOKEN
+        self.PADDING_TOKEN = PADDING_TOKEN
+    
+    def batch_tokenize(self, batch, start_token, end_token):
+
+        def tokenize(sentence, start_token, end_token):
+
+            sentence_word_indicies = [self.language_to_index[token] for token in list(sentence)]
+            if start_token:
+                sentence_word_indicies.insert(0, self.language_to_index[self.START_TOKEN])
+            if end_token:
+                sentence_word_indicies.append(self.language_to_index[self.END_TOKEN])
+            for _ in range(len(sentence_word_indicies), self.max_sequence_length):
+                sentence_word_indicies.append(self.language_to_index[self.PADDING_TOKEN])
+
+            return torch.tensor(sentence_word_indicies)
+
+        tokenized = []
+        for sentence_num in range(len(batch)):
+           tokenized.append( tokenize(batch[sentence_num], start_token, end_token) )
+        
+        # print(f"TOKENIZED: {tokenized}")
+        tokenized = torch.stack(tokenized)
+
+        return tokenized.to(get_device())
+    
+    def forward(self, x, start_token, end_token): # sentence
+
+        x = self.batch_tokenize(x, start_token, end_token)
+        x = self.embedding(x)
+        pos = self.position_encoder().to(get_device())
+        x = self.dropout(x + pos)
+        
+        return x
+
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, d_model, num_heads):
+
+        super().__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.head_dim = d_model // num_heads
+        self.qkv_layer = nn.Linear(d_model, 3 * d_model)
+        self.ln = nn.LayerNorm(d_model, d_model)
+
+    def forward(self, x, mask):
+
+        B, T, C = x.size()
+        
+        qkv = self.qkv_layer(x)
+        qkv = qkv.reshape(B, T, self.num_heads, 3 * self.head_dim)
+        qkv = qkv.permute(0, 2, 1, 3)
+
+        q, k, v = qkv.chunk(3, dim=-1)
+        values, attention = scaled_dot_product(q, k, v, mask)
+
+        values = values.permute(0, 2, 1, 3).reshape(B, T, self.num_heads * self.head_dim)
+        out = self.ln(values)
+
+        return out
+
+class FeedFoward(nn.Module):
+
+    def __init__(self, d_model, hidden, drop_prob=0.2):
+
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(d_model, hidden),
+            nn.ReLU(),
+            nn.Linear(hidden, d_model),
+            nn.Dropout(drop_prob)
+        )
+
+    def forward(self, x):
+
+        return self.net(x)
+    
+class EncoderBLock(nn.Module):
+
+    def __init__(self):
+        
+        super().__init__()
+
+class EncoderLayer(nn.Module):
+    
+    def __init__(self, d_model, ffn_hidden, num_heads, drop_prob):
+    
+        super(EncoderLayer, self).__init__()
+        self.attention = MultiHeadAttention(d_model=d_model, num_heads=num_heads)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(p=drop_prob)
+        self.ffn = FeedFoward(d_model=d_model, hidden=ffn_hidden, drop_prob=drop_prob)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout2 = nn.Dropout(p=drop_prob)
+
+    
+    def forward(self, x, self_attention_mask):
+
+        residual_x = x.clone()
+        x = self.attention(x, mask=self_attention_mask)
+        x = self.dropout1(x)
+        x = self.norm1(x + residual_x)
+        residual_x = x.clone()
+        x = self.ffn(x)
+        x = self.dropout2(x)
+        x = self.norm2(x + residual_x)
+
+        return x
+    
+class SequentialEncoder(nn.Sequential):
+
+    def forward(self, *inputs):
+
+        x, self_attention_mask  = inputs
+        for module in self._modules.values():
+            x = module(x, self_attention_mask)
+
+        return x
+    
+
+class Encoder(nn.Module):
+    def __init__(self, 
+                 d_model, 
+                 ffn_hidden, 
+                 num_heads, 
+                 drop_prob, 
+                 num_layers,
+                 max_sequence_length,
+                 language_to_index,
+                 START_TOKEN,
+                 END_TOKEN, 
+                 PADDING_TOKEN):
+        super().__init__()
+        self.sentence_embedding = SentenceEmbedding(max_sequence_length, d_model, language_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN)
+        self.layers = SequentialEncoder(*[EncoderLayer(d_model, ffn_hidden, num_heads, drop_prob)
+                                      for _ in range(num_layers)])
+
+    def forward(self, x, self_attention_mask, start_token, end_token):
+        x = self.sentence_embedding(x, start_token, end_token)
+        x = self.layers(x, self_attention_mask)
+        return x
+    
+class MultiHeadCrossAttention(nn.Module):
+
+    def __init__(self, d_model, num_heads):
+
+        super().__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.head_dim = d_model // num_heads
+
+        self.kv_layer = nn.Linear(d_model , 2 * d_model)
+        self.q_layer = nn.Linear(d_model , d_model)
+        self.linear_layer = nn.Linear(d_model, d_model)
+    
+    def forward(self, x, y, mask):
+
+        batch_size, sequence_length, d_model = x.size()
+        kv = self.kv_layer(x)
+        q = self.q_layer(y)
+
+        kv = kv.reshape(batch_size, sequence_length, self.num_heads, 2 * self.head_dim)
+        q = q.reshape(batch_size, sequence_length, self.num_heads, self.head_dim)
+        kv = kv.permute(0, 2, 1, 3)
+        q = q.permute(0, 2, 1, 3)
+        k, v = kv.chunk(2, dim=-1)
+        
+        values, attention = scaled_dot_product(q, k, v, mask)
+        values = values.permute(0, 2, 1, 3).reshape(batch_size, sequence_length, d_model)
+        
+        out = self.linear_layer(values)
+
+        return out
+
+class DecoderLayer(nn.Module):
+
+    def __init__(self, d_model, ffn_hidden, num_heads, drop_prob):
+
+        super(DecoderLayer, self).__init__()
+        self.self_attention = MultiHeadAttention(d_model=d_model, num_heads=num_heads)
+        self.layer_norm1 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(p=drop_prob)
+
+        self.encoder_decoder_attention = MultiHeadCrossAttention(d_model=d_model, num_heads=num_heads)
+        self.layer_norm2 = nn.LayerNorm(d_model)
+        self.dropout2 = nn.Dropout(p=drop_prob)
+
+        self.ffn = FeedFoward(d_model=d_model, hidden=ffn_hidden, drop_prob=drop_prob)
+        self.layer_norm3 = nn.LayerNorm(d_model)
+        self.dropout3 = nn.Dropout(p=drop_prob)
+
+    def forward(self, x, y, self_attention_mask, cross_attention_mask):
+
+        _y = y.clone()
+        y = self.self_attention(y, mask=self_attention_mask)
+        y = self.dropout1(y)
+        y = self.layer_norm1(y + _y)
+
+        _y = y.clone()
+        y = self.encoder_decoder_attention(x, y, mask=cross_attention_mask)
+        y = self.dropout2(y)
+        y = self.layer_norm2(y + _y)
+
+        _y = y.clone()
+        y = self.ffn(y)
+        y = self.dropout3(y)
+        y = self.layer_norm3(y + _y)
+
+        return y
+
+class SequentialDecoder(nn.Sequential):
+
+    def forward(self, *inputs):
+    
+        x, y, self_attention_mask, cross_attention_mask = inputs
+
+        for module in self._modules.values():
+            y = module(x, y, self_attention_mask, cross_attention_mask)
+        
+        return y
+    
+class Decoder(nn.Module):
+
+    def __init__(self, 
+                 d_model, 
+                 ffn_hidden, 
+                 num_heads, 
+                 drop_prob, 
+                 num_layers,
+                 max_sequence_length,
+                 language_to_index,
+                 START_TOKEN,
+                 END_TOKEN, 
+                 PADDING_TOKEN):
+        
+        super().__init__()
+        self.sentence_embedding = SentenceEmbedding(max_sequence_length, d_model, language_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN)
+        self.layers = SequentialDecoder(*[DecoderLayer(d_model, ffn_hidden, num_heads, drop_prob) for _ in range(num_layers)])
+
+    def forward(self, x, y, self_attention_mask, cross_attention_mask, start_token, end_token):
+
+        y = self.sentence_embedding(y, start_token, end_token)
+        y = self.layers(x, y, self_attention_mask, cross_attention_mask)
+        
+        return y
+    
+class Transformer(nn.Module):
+
+    def __init__(self, 
+                d_model, 
+                ffn_hidden, 
+                num_heads, 
+                drop_prob, 
+                num_layers,
+                max_sequence_length, 
+                trg_vocab_size,
+                src_to_index,
+                trg_to_index,
+                START_TOKEN, 
+                END_TOKEN, 
+                PADDING_TOKEN
+                ):
+        
+        super().__init__()
+        self.max_sequence_length = max_sequence_length
+        self.trg_vocab_size = trg_vocab_size
+
+        self.PADDING_TOKEN = PADDING_TOKEN
+        self.START_TOKEN = START_TOKEN
+        self.END_TOKEN = END_TOKEN
+        
+        self.encoder = Encoder(d_model, ffn_hidden, num_heads, drop_prob, num_layers, max_sequence_length, src_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN)
+        self.decoder = Decoder(d_model, ffn_hidden, num_heads, drop_prob, num_layers, max_sequence_length, trg_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN)
+        self.linear = nn.Linear(d_model, trg_vocab_size)
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    def forward(self, 
+                x, 
+                y, 
+                encoder_self_attention_mask=None, 
+                decoder_self_attention_mask=None, 
+                decoder_cross_attention_mask=None,
+                enc_start_token=False,
+                enc_end_token=False,
+                dec_start_token=False, 
+                dec_end_token=False): 
+        
+        x = self.encoder(x, encoder_self_attention_mask, start_token=enc_start_token, end_token=enc_end_token)
+        out = self.decoder(x, y, decoder_self_attention_mask, decoder_cross_attention_mask, start_token=dec_start_token, end_token=dec_end_token)
+        out = self.linear(out)
+
+        return out
+    
+    def translate(self, transformer, src_sentence, trg_index_to_token):
+        
+        self.eval()
+        max_sequence_length = self.max_sequence_length
+        device = get_device()
+        src_sentence = (src_sentence,)
+        trg_sentence = ("",)
+        for word_counter in range(max_sequence_length):
+            encoder_self_attention_mask, decoder_self_attention_mask, decoder_cross_attention_mask=create_masks(src_sentence, trg_sentence)
+            predictions = transformer(src_sentence,
+                                    trg_sentence,
+                                    encoder_self_attention_mask.to(device), 
+                                    decoder_self_attention_mask.to(device), 
+                                    decoder_cross_attention_mask.to(device),
+                                    enc_start_token=False,
+                                    enc_end_token=False,
+                                    dec_start_token=True,
+                                    dec_end_token=False)
+            next_token_prob_distribution = predictions[0][word_counter]
+            next_token_index = torch.argmax(next_token_prob_distribution).item()
+            next_token = trg_index_to_token[next_token_index]
+            kn_sentence = (kn_sentence[0] + next_token, )
+            if next_token == self.END_TOKEN:
+                break
+        return kn_sentence[0]
+
